dc389c689c546afcbba191d38e11efadafc1fc91
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-17 15:47:30 +0200

    add cards with disc.json

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,16 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            discs: [],
+            api_url: 'disc.json'
         }
+    },
+    mounted() {
+        axios.get(this.api_url).then((result) => {
+            console.log(result)
+            this.discs = result.data
+            console.log(this.discs)
+        })
     }
 }).mount('#app')
--- index.php
+++ index.php
@@ -23,25 +23,32 @@ include 'api.php';
         <header>
             <img src="./assets/img/spotify.png" alt="">
         </header>
+
+
         <main>
             <div class="container pt-5">
-                <div class="col-3">
-                    <div class="card" v-for="disc in discs">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">{{disc.title}}</h4>
-                            <p class="card-text">Text</p>
+                <div class="row">
+                    <div class="col-4" v-for="disc in discs">
+                        <div class="card w-75 mb-3">
+                            <img class="card-img-top" :src="disc.poster" alt="Poster" />
+                            <div class="card-body">
+                                <h4 class="card-title">{{ disc.title }}</h4>
+                                <p class="card-text">{{ disc.author }}</p>
+                                <p class="card-text">{{ disc.genre }}</p>
+                                <p class="card-text">{{ disc.year }}</p>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </main>
     </div>
 </body>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js' integrity='sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
 
 <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js' integrity='sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
+
 
 <script src='./assets/js/app.js'></script>
 
